b957fa50921a2b0ed2da4e0207101388eaadacc0
david <hekkgs@gmai.com>
2017-07-24 15:17:33 +0800

    第二層JSON物件格式確定是String,改用for loop解出

--- cloud/ViewController.swift
+++ cloud/ViewController.swift
@@ -28,36 +28,44 @@ class ViewController: UIViewController {
                         print(value)
                     }
                     if let headers_dictionary = dictionary["headers"] as? [String:Any]{
-                        if let value = headers_dictionary["Accept"] as? String{
-                            print("解出key為Accept的值")
-                            print(value)
                         
+                        for (key,value) in headers_dictionary {
+                            if let value_string = value as? String {
+                                print(key + ":" + value_string)
+                            }
                         }
-                        if let value = headers_dictionary["Connection"] as? String{
-                            print("解出key為Connection的值")
-                            print(value)
-                            
-                        }
-                        if let value = headers_dictionary["Accept-Encoding"] as? String{
-                            print("解出key為Accept-Encoding的值")
-                            print(value)
-                            
-                        }
-                        if let value = headers_dictionary["Accept-Language"] as? String{
-                            print("解出key為Accept-Language的值")
-                            print(value)
-                            
-                        }
-                        if let value = headers_dictionary["Host"] as? String{
-                            print("解出key為Host的值")
-                            print(value)
-                            
-                        }
-                        if let value = headers_dictionary["User-Agent"] as? String{
-                            print("解出key為User-Agent的值")
-                            print(value)
-                            
-                        }
+                        
+//                        
+//                        if let value = headers_dictionary["Accept"] as? String{
+//                            print("解出key為Accept的值")
+//                            print(value)
+//                        
+//                        }
+//                        if let value = headers_dictionary["Connection"] as? String{
+//                            print("解出key為Connection的值")
+//                            print(value)
+//                            
+//                        }
+//                        if let value = headers_dictionary["Accept-Encoding"] as? String{
+//                            print("解出key為Accept-Encoding的值")
+//                            print(value)
+//                            
+//                        }
+//                        if let value = headers_dictionary["Accept-Language"] as? String{
+//                            print("解出key為Accept-Language的值")
+//                            print(value)
+//                            
+//                        }
+//                        if let value = headers_dictionary["Host"] as? String{
+//                            print("解出key為Host的值")
+//                            print(value)
+//                            
+//                        }
+//                        if let value = headers_dictionary["User-Agent"] as? String{
+//                            print("解出key為User-Agent的值")
+//                            print(value)
+//                            
+//                        }
                         
                        
                     }
@@ -65,6 +73,7 @@ class ViewController: UIViewController {
                         print("解出key為args的值")
                         print(value)
                     }
+                    
                     
                 }
                 
