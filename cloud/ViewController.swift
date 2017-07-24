51e707f9faf0a87941bc94815a5c583403e2810c
david <hekkgs@gmai.com>
2017-07-24 13:55:36 +0800

    網路傳輸套件，初步測試

--- cloud/ViewController.swift
+++ cloud/ViewController.swift
@@ -1,23 +1,22 @@
-//
-//  ViewController.swift
-//  cloud
-//
-//  Created by s1 on 2017/7/24.
-//  Copyright © 2017年 ho. All rights reserved.
-//
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        Alamofire.request("https://httpbin.org/get").responseJSON{response in
+            print(response.request as Any)
+            print(response.response as Any)
+            print(response.data as Any)
+            print(response.result)
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+   
     }
 
 
